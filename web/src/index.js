2748ffd7583ddc80fd7e0edcb997bd1545374732
PandawanFr <pandawanfr@gmail.com>
2019-06-29 13:51:18 -0700

    first agora stuff

--- web/src/index.js
+++ web/src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import AgoraRTC from 'agora-rtc-sdk';
+import creds from './creds.json';
+
+const client = AgoraRTC.createClient({ mode: 'live', codec: 'h264' });
+
+client.init(creds.agora.appid, function () {
+  console.log('AgoraRTC client initialized');
+}, function (err) {
+  console.log('AgoraRTC client init failed', err);
+});
