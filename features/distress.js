f0a849b7a2a0d419c495235f494607d465d6f044
PandawanFr <pandawanfr@gmail.com>
2019-06-30 01:24:56 -0700

    UwU

--- features/distress.js
+++ features/distress.js
@@ -7,7 +7,7 @@ module.exports = function (controller) {
   });
 
   controller.on('message', async (bot, message) => {
-    console.log(message.attachments[0].payload);
+    console.log(JSON.stringify(message, '', '\t'));
     await bot.reply(message, {
       text: 'Hello. Please share your location so help can arrive as soon as possible.',
       quick_replies: [
