3f5413b69ad5620502aba4dcbd961e51e2fcec32
PandawanFr <pandawanfr@gmail.com>
2019-06-30 12:01:36 -0700

    WOW

--- features/distress.js
+++ features/distress.js
@@ -18,7 +18,7 @@ module.exports = async function (controller) {
           let insertion = `INSERT INTO Points
                         (title, latitude, longitude, tags, id)
                         VALUES 
-                        ('${tempStorage.message}', ${coordinates.lat},  ${coordinates.long},  ${tempStorage.watsonData}, ${Math.round(Math.random() * 10000)});`;
+                        ('${tempStorage.message}', ${coordinates.lat},  ${coordinates.long},  '${tempStorage.watsonData}', ${Math.round(Math.random() * 10000)});`;
           console.log(insertion);
 
           const pool = new Pool({
