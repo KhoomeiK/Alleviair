3382d7084f9c644c5aff3231122e76ff5eb23c30
PandawanFr <pandawanfr@gmail.com>
2019-06-29 19:45:41 -0700

    Add everything through botkit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Alleviair
+
 Disaster Alleviation via Air
+
+# Botkit Starter Kit
+
+This is a Botkit starter kit for facebook, created with the [Yeoman generator](https://github.com/howdyai/botkit/tree/master/packages/generator-botkit#readme)
+
+[Botkit Docs](https://botkit.ai/docs/v4)
+
+This bot is powered by [a folder full of modules](https://botkit.ai/docs/v4/core.html#organize-your-bot-code).
+Edit the samples, and add your own in the [features/](features/) folder.
---
+++ bot.js
@@ -0,0 +1,64 @@
+//  __   __  ___        ___
+// |__) /  \  |  |__/ |  |
+// |__) \__/  |  |  \ |  |
+
+// This is the main file for the alleviair bot.
+
+const path = require('path');
+
+// Import Botkit's core features
+const { Botkit } = require('botkit');
+const { BotkitCMSHelper } = require('botkit-plugin-cms');
+
+// Import a platform-specific adapter for facebook.
+
+const { FacebookAdapter, FacebookEventTypeMiddleware } = require('botbuilder-adapter-facebook');
+
+// Load process.env values from .env file
+require('dotenv').config();
+
+let storage = null;
+
+const adapter = new FacebookAdapter({
+  verify_token: process.env.FACEBOOK_VERIFY_TOKEN,
+  access_token: process.env.FACEBOOK_ACCESS_TOKEN,
+  app_secret: process.env.FACEBOOK_APP_SECRET
+});
+
+// emit events based on the type of facebook event being received
+adapter.use(new FacebookEventTypeMiddleware());
+
+const controller = new Botkit({
+  debug: true,
+  webhook_uri: '/api/messages',
+
+  adapter: adapter,
+
+  storage
+});
+
+if (process.env.cms_uri) {
+  controller.usePlugin(new BotkitCMSHelper({
+    cms_uri: process.env.cms_uri,
+    token: process.env.cms_token
+  }));
+}
+
+// Once the bot has booted up its internal services, you can use them to do stuff.
+controller.ready(() => {
+  // load traditional developer-created local custom feature modules
+  controller.loadModules(path.join(__dirname, '/features'));
+
+  /* catch-all that uses the CMS to trigger dialogs */
+  if (controller.plugins.cms) {
+    controller.on('message,direct_message', async (bot, message) => {
+      let results = false;
+      results = await controller.plugins.cms.testTrigger(bot, message);
+
+      if (results !== false) {
+        // do not continue middleware!
+        return false;
+      }
+    });
+  }
+});
