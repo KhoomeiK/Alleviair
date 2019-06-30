a395029a2985509ef150562a948c6cd364db3876
PandawanFr <pandawanfr@gmail.com>
2019-06-30 01:02:35 -0700

    Starting distress bot

---
+++ features/distress.js
@@ -0,0 +1,21 @@
+/**
+ * @param {import('botkit').Botkit} controller
+ */
+module.exports = function (controller) {
+  controller.on('message', async (bot, message) => {
+    await bot.reply(message, {
+      text: 'Hello. Please share your location so emergency services can arrive as soon as possible.',
+      quick_replies: [
+        {
+          content_type: 'location'
+        }
+      ]
+    });
+  });
+
+  controller.on('facebook_postback', async (bot, message) => {
+    await bot.reply(message, {
+      text: JSON.stringify(message)
+    });
+  });
+};
--- webserver.js
+++ webserver.js
@@ -3,5 +3,7 @@
  * @param {import('express').Application} server
  */
 module.exports = function (server) {
-
+  server.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
 };
