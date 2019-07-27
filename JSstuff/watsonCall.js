bdcb039fb7308473a93ad1d7fefebb4f087e06da
KhoomeiK <rohan.pandey@gmail.com>
2019-07-26 19:17:58 -0700

    awaitfix

--- JSstuff/watsonCall.js
+++ JSstuff/watsonCall.js
@@ -2,6 +2,7 @@
 module.exports = async function (userText){
   try{
     const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js');
+    
     const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
       version: '2018-11-16',
       iam_apikey: 'Ohosf4bP5cVpnI125et2u4pMw592bhrZ1xU-502dxKYF',
@@ -31,13 +32,10 @@ module.exports = async function (userText){
 
 
   let str = "";
-  try{
-    naturalLanguageUnderstanding.analyze(analyzeParams)
-      .then(analysisResults => {
-        splitted = analysisResults.keywords.map(obj => obj.text);
-        out = splitted.join();
-      return out;
-      })
+  try {
+    let res = await naturalLanguageUnderstanding.analyze(analyzeParams);
+    let out = res.keywords.map(obj => obj.text).join();
+    return out;
   }
 
   catch(err) {
