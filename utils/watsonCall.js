b6c070053b79f5820867320284a459b6e68dad1f
KhoomeiK <rohan.pandey@gmail.com>
2019-07-26 20:07:52 -0700

    logs

--- utils/watsonCall.js
+++ utils/watsonCall.js
@@ -1,7 +1,7 @@
 module.exports = async function (userText){
   const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1.js');
   
-  const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+  const nlu = new NaturalLanguageUnderstandingV1({
     version: '2018-11-16',
     iam_apikey: 'Ohosf4bP5cVpnI125et2u4pMw592bhrZ1xU-502dxKYF',
     url: 'https://gateway.watsonplatform.net/natural-language-understanding/api'
@@ -19,13 +19,14 @@ module.exports = async function (userText){
   };
 
   try {
-    let res = await naturalLanguageUnderstanding.analyze(analyzeParams);
+    let res = await nlu.analyze(analyzeParams);
     let out = res.keywords.map(obj => obj.text).join();
+    console.log(out);
     return out;
   }
   catch(err) {
     console.log(error);
-    return 'Error'
+    return 'Error';
   }
 }
 
